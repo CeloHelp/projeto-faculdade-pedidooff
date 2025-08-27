3e6ba806dd4ca1e47e9c1ba29baa68ba1f3b81e1
Marcelo <marcelopacobello@gmail.com>
2025-08-27 13:54:13 -0300

    chore: update module-info requires for spring-data-jpa and tx

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -8,9 +8,13 @@ module com.pedidooff {
     requires spring.context;
     requires spring.beans;
     requires spring.core;
+    requires spring.data.jpa;
+    requires spring.tx;
     requires jakarta.persistence;
 
     opens com.pedidooff to javafx.fxml, spring.core, spring.beans, spring.context;
+    opens com.pedidooff.service to spring.core, spring.beans, spring.context;
+    opens com.pedidooff.repository to spring.core, spring.beans, spring.context;
     opens com.pedidooff.model to org.hibernate.orm.core, spring.core, spring.beans, spring.context;
 
     exports com.pedidooff;
